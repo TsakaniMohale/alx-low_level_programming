b2a15a970cc9a32708411312c9a8f75ccb134645
Tsakani Mohale <tsakanimohale4@gmail.com>
2023-02-28 05:37:29 -0800

    minor changes

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - resets the integer value stored at the memory.
- *
+ * reset_to_98 - resets the integer value stored at the memory.*
  * @n:a pointer to an integer as the functions input parameter.
- *
  */
 
 void reset_to_98(int *n)
