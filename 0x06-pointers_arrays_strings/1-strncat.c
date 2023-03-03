49b1c5dc421597f125b94edc81164a6369189cbc
Tsakani Mohale <tsakanimohale4@gmail.com>
2023-03-03 13:23:00 -0800

    redone code

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,19 +10,11 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int c, a;
+	int index = 0, dest_len = 0;
 
-	c = 0;
-	a = 0;
-
-	while (dest[a])
-		c++;
-	 
-	for (a = 0;a < n && src[a]; a++)
-	{
-		dest[c++] = src[a];
-
-	}
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
-
