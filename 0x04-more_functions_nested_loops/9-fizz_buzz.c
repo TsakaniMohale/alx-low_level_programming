0e6a313fc33c92f33eeeae65f785a19850f9042e
Tsakani Mohale <tsakanimohale4@gmail.com>
2023-07-20 14:42:38 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,19 @@ int main(void)
 {
 	int i;
 
-	for (i =1; i <=100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if(i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("Fizzbuzz ");
 		}
 		else if (i % 5 == 0)
 		{
-			if(i != 100)
+			if (i != 100)
 			{
 				printf("Buzz ");
 			}
-			else 
+			else
 			{
 				printf("fizz");
 			}
