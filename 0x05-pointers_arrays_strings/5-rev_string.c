99a1a7470b0e983a0f1133f9815244fc7d864589
Tsakani <tsakanimohale4@gmail.com>
2023-07-26 04:55:42 +0200

    5.A good engineer thinks in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * reverse_string - Reverse a given string in place.
+ *
+ * This function takes a string as input and reverse a given string in place.
+ *
+ * @s: The input string to be reserved
+ */
+
+void reverse_string(char *s)
+{
+	int length = 0;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (int a = 0; a < length / 2; a++)
+	{
+		temp = s[a];
+		s[a] = s[length - 1 - a];
+		s[length - 1 - a] = temp;
+	}
+}
