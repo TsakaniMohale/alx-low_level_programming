153eaa9cb27bf549db6552fe6eae4e8aa0e39748
Tsakani Mohale <tsakanimohale4@gmail.com>
2023-07-31 17:37:06 -0700

    8. Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - Set a pointer to to point a string
+ * @s: Pointer to a pointer
+ * @to: Pointer to a string
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
