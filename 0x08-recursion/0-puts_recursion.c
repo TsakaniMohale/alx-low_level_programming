346addf1128727007ab59584de72839d7da715f0
Tsakani Mohale <tsakanimohale4@gmail.com>
2023-08-02 14:10:01 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Printing characters in a string using recursion.
+ *
+ * @s: Pointer to the string.
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[1] != '\0')
+	{
+		_putchar(s[i]);
+		_puts_recursion(s + 1);
+	} else
+	{
+                _putchar('\n');
+	}
+}
