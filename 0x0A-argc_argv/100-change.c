a077602194fd3832cfd305e227d77d4b4477ad0b
Tsakani <tsakanimohale4@gmail.com>
2023-08-08 14:26:24 +0200

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,37 +1,43 @@
 #include "main.h"
 
 /**
- * main - This program prints minimun number of coins..
- * @argc: values of the coins..
- * @argv: Values of the number of changes.
- * Return: Return 0 on success and 1 on failure.
+ * main - Execution start, calculate change with min coins.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ * Return: 0 or 1, on success or failure respectively.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int money[] = {25, 10, 5, 2, 1};
-	int amount, min_c_change, a;
-	
+	int array_coins[] = {25, 10, 5, 2, 1};
+	int tot_amnt, min_coins, i;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
+
 		return (1);
 	}
-	amount = atoi(argv[1]);
-	min_c_change = 0;
-	if (amount < 0)
+	tot_amnt = atoi(argv[1]);
+	min_coins = 0;
+
+	if (tot_amnt < 0)
 	{
-		printf("0\n");
-		return (0);
-	}
-	for (a = 0; a < 5 && amount >= 0; a++)
-	{
-		while (amount >= money[a])
-		{
-			min_c_change++;
-			amount = amount - money[a];
-		}
-		printf("%d\n", min _c_change);
+		printf("\n");
+
 		return (0);
 	}
 
+	for (i = 0; i < 5 && tot_amnt >= 0; i++)
+	{
+		while (tot_amnt >= array_coins[i])
+		{
+			min_coins++;
+			tot_amnt = tot_amnt - array_coins[i];
+		}
+	}
+
+	printf("%d\n", min_coins);
+
+	return (0);
+}
