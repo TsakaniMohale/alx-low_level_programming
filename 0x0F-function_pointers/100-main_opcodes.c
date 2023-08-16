1a1624a3c00b30f457dc63ce50029e976d3b1b8c
Tsakani <tsakanimohale4@gmail.com>
2023-08-17 00:45:12 +0200

    4. Most hackers are young because young people tend to be adaptable

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -7,20 +6,26 @@
  *
  * print_opcodes - program that prints the opcodes of its own main function
  *
- * Return; 0 on success or 1 , 2 on a failure
+ * @argc: Total number of commands agrguments
+ *
+ * @argv: Values of commands argument
+ *
+ * Return: 0 on success or 1 , 2 on a failure
  */
-
-void print_opcodes(void);
 
 int main(int argc, char *argv[])
 {
+	int a, n_bytes;
+	int (*opcodes)(int, char **);
+
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int num_bytes = atoi(argv[1]);
+	n_bytes = atoi(argv[1]);
 
 	if (num_bytes <= 0)
 
@@ -29,13 +34,19 @@ int main(int argc, char *argv[])
 		return (2);
 	}
 
-	void print_opcodes(void) /*prints the ocodes of the main function*/
-	{
-		unsigned char *ptr = (unsigned char *)print_pocodes;
+	opcodes = main;
 
-		for (int i = 0; i < (int)print_opcodes - (int)main; i++)
-			printf("%02x", ptr[i]);
+	for (a = 0; a < n_bytes; i++)
+
+	{
+		printf("%02x", *(unsigned char *)opcodes);
+
+		if (a == nbytes - 1)
+			continue;
+		printf(" ");
+		opcodes++;
 	}
 
 	printf("\n");
+	return (0);
 }
