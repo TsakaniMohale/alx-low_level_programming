dc3415124e0c4e523c285168e31a4d762ea8d0c4
Tsakani Mohale <tsakanimohale4@gmail.com>
2023-08-27 19:16:18 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,20 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	size_t start = 0;
 
 	while (h != NULL)
 	{
 		if (h->str != NULL)
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 		else
+		{
 			printf("[0] (nil)\n");
-		n++;
+		}
+		start += 1;
 		h = h->next;
 	}
-	return (n);
+	return (start);
 }
