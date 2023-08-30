094d583a6b3fefeef4846038102b44e36d672dfb
Tsakani Mohale <tsakanimohale4@gmail.com>
2023-08-30 09:38:52 -0700

    0. Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Output the numbers
+ * @separator:n separate the printed numbers
+ * @n: Total number of arguments passed
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i;
+	
+	va_start(Ptr, n);
+	
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(Ptr, int));
+		if (separator && i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+}
