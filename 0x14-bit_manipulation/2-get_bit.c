9c0d40b4eb2b753c4f4407d20daeeca7af82a21c
Tsakani Mohale <tsakanimohale4@gmail.com>
2023-10-19 03:33:06 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - Get a value of a bit
+ * @n: Integer to to extract a bit 
+ * @index: Position of a bit
+ * Return: A bit 1 or 0 and or -1 on failure
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	return ((n >> index) & 1);
+}
