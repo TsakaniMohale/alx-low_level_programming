c0d68f786f9d53dfb0e536c871e8136e113256cd
Tsakani Mohale <tsakanimohale4@gmail.com>
2023-10-19 03:34:17 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * get_bit - Get a value of a bit
- * @n: Integer to to extract a bit 
+ * @n: Integer to to extract a bit
  * @index: Position of a bit
  * Return: A bit 1 or 0 and or -1 on failure
  */
